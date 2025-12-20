92d052e5d5bc24c829929da147d1e89c3e7b5581
backendcodigix <backendcodigix@gmail.com>
2025-12-20 11:05:28 +0530

    update in database.js

--- deals-dashboard/server/config/database.js
+++ deals-dashboard/server/config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const path = require('path');
 const dotenv = require('dotenv');
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV;
 const envFile = NODE_ENV === 'production' ? '.env.production' : '.env.development';
 dotenv.config({ path: path.join(__dirname, '..', envFile) });
 
